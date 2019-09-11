a5da1fa511e16b88d72a35d972f35b9233f971eb
alam-mashrufee <mashrufee@gmail.com>
2019-09-11 15:03:27 +0900

    initial settings done

--- AWStest/settings.py
+++ AWStest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2dxti8uaiv80w6fpq2v82!o115-l!8d-bydumuko2u#ir-s!tb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -120,18 +121,18 @@ USE_TZ = True
 
 #AWS settings
 
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
-AWS_STORAGE_BUCKET_NAME = ''
-AWS_S3_CUSTOM_DOMAIN = ''
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % config('AWS_STORAGE_BUCKET_NAME') 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl':'max-age=86400'
 }
-AWS_LOCATION = 'static'
+AWS_LOCATION = config('AWS_LOCATION')
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'AWStest/static')
 ]
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN,AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (config('AWS_S3_CUSTOM_DOMAIN'), config('AWS_LOCATION'))
+STATICFILES_STORAGE = config('STATICFILES_STORAGE')
